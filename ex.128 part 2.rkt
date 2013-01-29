08c81d7ec636f9ec1d8c1eb57ef4eb6d9773145a
Andrew Tran <atran246@gmail.com>
2013-01-29 12:49:20 -0600

    Add Data Definition

--- ex.128 part 2.rkt
+++ ex.128 part 2.rkt
@@ -1,3 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |ex.128 part 2|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+;Andrew Tran Add Function
+
+; A N (Natural Number) is one of:
+; - 0
+; - (add1 N)
