703036a59a367b49b168c6db6e9fbb40198ecba3
Andrew Tran <atran246@gmail.com>
2013-01-29 12:36:20 -0600

    Ex 128 Data Definitions

---
+++ ex. 128.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname |ex. 128|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; Andrew Tran Ex 128
+
+; A N (Natural Number) is one of:
+; - 0
+; - (add1 N)
